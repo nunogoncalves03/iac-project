dd4e5ab94b0e69f9ba792f58dc5a1c087ac937a3
Nuno Gonçalves <93784204+nunogoncalves03@users.noreply.github.com>
2022-06-01 16:40:09 +0100

    Update projeto.asm

--- projeto.asm
+++ projeto.asm
@@ -10,10 +10,13 @@
 DISPLAYS   			EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
 TEC_LIN				EQU 0C000H	; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL				EQU 0E000H	; endereço das colunas do teclado (periférico PIN)
-LINHA_TECLADO		EQU 8		; linha a testar (4ª linha, 1000b)
+LINHA_1_TECLADO		EQU 0001b	; linha 1 do teclado
+LINHA_2_TECLADO		EQU 0010b	; linha 2 do teclado
+LINHA_3_TECLADO		EQU 0100b	; linha 3 do teclado
+LINHA_4_TECLADO		EQU 1000b	; linha 4 do teclado
+COLUNA_1_TECLADO	EQU 0001b	; coluna 1 do teclado
+COLUNA_2_TECLADO	EQU 0010b	; coluna 2 do teclado
 MASCARA				EQU 0FH		; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-TECLA_ESQUERDA		EQU 1		; tecla na primeira coluna do teclado (tecla 0)
-TECLA_DIREITA		EQU 2		; tecla na segunda coluna do teclado (tecla 1)
 
 DEFINE_LINHA    		EQU 600AH	; endereço do comando para definir a linha
 DEFINE_COLUNA   		EQU 600CH   ; endereço do comando para definir a coluna
@@ -23,12 +26,12 @@ APAGA_ECRÃ	 			EQU 6000H   ; endereço do comando para apagar todos os pixels e
 APAGA_ECRÃS	 			EQU 6002H   ; endereço do comando para apagar todos os pixels já desenhados
 SELECIONA_CENARIO_FUNDO EQU 6042H   ; endereço do comando para selecionar uma imagem de fundo
 SELECIONA_ECRÃ		 	EQU 6004H   ; endereço do comando para selecionar o ecrã
-TOCA_SOM				EQU 605AH      ; endereço do comando para tocar um som
+TOCA_SOM				EQU 605AH   ; endereço do comando para tocar um som
 
 MAX_LINHA		EQU  31     ; número da linha mais abaixo que o objeto pode ocupar
 MIN_COLUNA		EQU  0		; número da coluna mais à esquerda que o objeto pode ocupar
 MAX_COLUNA		EQU  63     ; número da coluna mais à direita que o objeto pode ocupar
-ATRASO			EQU	1600H	; atraso para limitar a velocidade de movimento do boneco
+ATRASO			EQU	1800H	; atraso para limitar a velocidade de movimento do boneco
 
 LARGURA_ROVER	EQU	5 		; largura do boneco
 ALTURA_ROVER	EQU 4 		; altura do boneco
@@ -42,7 +45,9 @@ COR_METEORO 	EQU 0FF00H  ; cor do meteoro: vermelho em ARGB (opaco e vermelho no
 LINHA_METEORO   EQU 0       ; linha do meteoro (a meio do ecrã)
 COLUNA_METEORO	EQU 30      ; coluna do meteoro (a meio do ecrã)
 
-ENERGIA		EQU 50 		; energia
+ENERGIA_INICIAL	EQU 50 			; valor inicial da energia (em decimal)
+ENERGIA_MÍNIMA  EQU 0    		; valor mínimo de energia (em decimal)
+ENERGIA_MÁXIMA  EQU 100    		; valor máximo de energia (em decimal)
 
 ; ******************************************************************************
 ; * Dados 
@@ -74,175 +79,116 @@ DEF_METEORO:
 ; ******************************************************************************
 ; * Código
 ; ******************************************************************************
-	PLACE   0				; o código tem de começar em 0000H
+	PLACE   0000H			; o código tem de começar em 0000H
 inicio:
-	MOV  SP, SP_inicial		; inicializa SP para a palavra a seguir
-							; à última da pilha
-                            
+	MOV  SP, SP_inicial		; inicializa SP para a palavra a seguir à última da pilha
 	MOV  [APAGA_AVISO], R1	; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
 	MOV  [APAGA_ECRÃS], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
 	MOV	 R1, 0				; cenário de fundo número 0
 	MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
-
+	MOV  R6, LINHA_4_TECLADO  	; primeira linha a testar (4ª linha)
+inicializa_meteoro:
 	MOV  R1, 1
 	MOV  [SELECIONA_ECRÃ], R1   ; seleciona ecrã 1
 	MOV  R1, LINHA_METEORO		; linha do meteoro
 	MOV  R2, COLUNA_METEORO		; coluna do meteoro
 	MOV	 R4, DEF_METEORO		; endereço da tabela que define o meteoro
 	CALL	desenha_boneco		; desenha o meteoro a partir da tabela
-	MOV  R8, R1					; cópia linha
-	MOV  R9, R2					; cópia coluna
-	MOV  R10, R4				; cópia DEF_METEORO
-
+	MOV  R8, R1					; cópia da linha do meteoro
+	MOV  R9, R2					; cópia da coluna do meteoro
+	MOV  R10, R4				; cópia da endereço da tabela que define o meteoro
+inicializa_rover:
 	MOV  R1, 0
 	MOV  [SELECIONA_ECRÃ], R1   ; seleciona ecrã 0
     MOV  R1, LINHA_ROVER	  	; linha do rover
 	MOV  R2, COLUNA_ROVER	  	; coluna do rover
 	MOV	 R4, DEF_ROVER		  	; endereço da tabela que define o rover
-
-	MOV  R6, LINHA_TECLADO  	; linha a testar (4ª linha, 1000b)
-	MOV	 R7, 1					; valor a somar à coluna do boneco, para o movimentar
-	MOV  R11, ENERGIA 			;
-	CALL mostra_energia
-
-mostra_rover:
 	CALL	desenha_boneco		; desenha o rover a partir da tabela
+inicializa_displays:
+	MOV  R11, ENERGIA_INICIAL 	; valor inicial da energia (em decimal)
+	CALL mostra_energia			; mostra a energia do rover nos displays
 
-tecla_premida:               ; verificar se uma tecla de movimento ainda está a ser premida
-    CALL teclado
-    CMP  R0, 0         		 ; há tecla premida?
-    JNZ  testa_esquerda      ; se ainda houver uma tecla premida, move o rover
-    JMP  espera_tecla
-
-move_meteoro:
-	CALL	atraso
-
-	;CALL	apaga_boneco		; apaga o boneco na sua posição corrente
-	PUSH R10
-	MOV  R10, 1
-	MOV  [APAGA_ECRÃ], R10 		; apaga ecrã 1
-	MOV  [SELECIONA_ECRÃ], R10  ; seleciona ecrã 1
-	POP  R10
-mostra_meteoro:
-	PUSH  R1
-	PUSH  R2
-	PUSH  R3
-	PUSH  R4
-
-	MOV  R3, 0
-	MOV  R1, R8					; cópia linha
-	MOV  R2, R9					; cópia coluna
-	MOV  R4, R10				; cópia DEF_METEORO
-	CALL	desenha_boneco		; desenha o meteoro a partir da tabela
-	MOV [TOCA_SOM], R3			; comando para tocar o som
-	MOV  [SELECIONA_ECRÃ], R3   ; seleciona ecrã 0
-
-	POP R4
-	POP R3
-	POP R2
-	POP R1
-	JMP ha_tecla
 
 espera_tecla:				; neste ciclo espera-se até uma tecla ser premida
 	CALL	teclado			; leitura às teclas
 	CMP	 R0, 0
-	JZ	 tec_proxima_linha	; nenhuma tecla detectada, proxima linha
-	JMP  processamento_linha
-tec_proxima_linha:
-    SHR  R6, 1       		; preparar próxima linha
-    JNZ  espera_tecla  		;
-    MOV  R6, LINHA_TECLADO  ;
-    JMP  espera_tecla  		;
+	JZ	 proxima_linha 		; nenhuma tecla detectada, proxima linha
+	JMP  processa_linha
+proxima_linha:
+    SHR  R6, 1       			; preparar próxima linha
+    JNZ  espera_tecla  			;
+    MOV  R6, LINHA_4_TECLADO	;
+    JMP  espera_tecla  			;
+processa_linha:
+	CMP  R6, LINHA_1_TECLADO	; linha 1
+	JZ   testa_linha_1			; há tecla premida na linha 1?
+	CMP  R6, LINHA_2_TECLADO	; linha 2
+	JZ   testa_linha_2
+	CMP  R6, LINHA_3_TECLADO	; linha 3
+	JZ   testa_linha_3
+	JMP  espera_tecla			; linha 4
 ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
     CALL	teclado
     CMP  R0, 0         ; há tecla premida?
     JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera até não haver
     JMP  espera_tecla
-processamento_linha:
-	CMP  R6, 1 		; linha 1
-	JZ   testa_esquerda
-	CMP  R6, 2 		; linha 2
-	JZ   testa_linha_2
-	CMP  R6, 4 		; linha 3
-	JZ   testa_linha_3
-	JMP  espera_tecla
 
-testa_esquerda:
-	CMP	R0, TECLA_ESQUERDA 	; tecla 0
-	JNZ	testa_direita
-	MOV	R7, -1			; vai deslocar para a esquerda
+
+testa_linha_1:
+testa_tecla_esquerda: 		; testa a tecla que move o rover para a esquerda (tecla 0)
+	CMP	R0, COLUNA_1_TECLADO; tecla 0
+	JNZ	testa_tecla_direita ; tecla esquerda não premida? 
+	MOV	R7, -1				; vai deslocar para a esquerda
 	JMP	ve_limites_horizontal
-testa_direita:
-	CMP	R0, TECLA_DIREITA 	; tecla 1
-	JNZ	espera_tecla		; tecla que não interessa
-	MOV	R7, +1			; vai deslocar para a direita
+testa_tecla_direita: 		; testa a tecla que move o rover para a direita (tecla 1)
+	CMP	R0, COLUNA_2_TECLADO; tecla 1
+	JNZ	espera_tecla		; nenhuma tecla de movimento premida?
+	MOV	R7, +1				; vai deslocar para a direita
 ve_limites_horizontal:
-	CALL	testa_limites	; vê se chegou aos limites do ecrã e se sim força R7 a 0
+	CALL	testa_limites_horizontal	; vê se chegou aos limites do ecrã e se sim força R7 a 0
 	CMP	R7, 0
 	JZ	espera_tecla		; se não é para movimentar o objeto, vai ler o teclado de novo
+	CALL move_rover
+tecla_premida:               ; verifica se uma tecla de movimento ainda está a ser premida
+    CALL teclado 			 ; lê as teclas da linha (armazenada em R6)
+    CMP  R0, 0         		 ; há tecla premida?
+    JNZ  testa_linha_1		 ; se ainda houver uma tecla premida, move o rover
+    JMP  espera_tecla		 ; caso contrário, espera-se até uma tecla ser premida
 
-move_boneco:
-	CALL	atraso
-
-	;CALL	apaga_boneco		; apaga o boneco na sua posição corrente
-	PUSH R10
-	MOV  R10, 0
-	MOV  [APAGA_ECRÃ], R10
-	POP  R10
-coluna_seguinte:
-	ADD	R2, R7			; para desenhar objeto na coluna seguinte (direita ou esquerda)
-
-	JMP	mostra_rover	; vai desenhar o boneco de novo
 
 testa_linha_2:
-    CMP  R0, 1H         ; tecla 4
-    JZ   decremento
-    CMP  R0, 2H 		; tecla 5
-    JZ   incremento
-    JMP  espera_tecla
-incremento:
+    CMP  R0, COLUNA_1_TECLADO   ; tecla que decrementa a energia do rover (tecla 4)
+    JZ   decrementa_energia
+    CMP  R0, COLUNA_2_TECLADO 	; tecla que incrementa a energia do rover (tecla 5)
+    JZ   incrementa_energia
+    JMP  espera_tecla 			; nenhuma tecla de incremento/decremento de energia premida?
+incrementa_energia:
 	PUSH R0
-	MOV  R0, 100
-	CMP  R11, R0
-	JGE  sai_incremento
-    ADD  R11, 1
-    CALL mostra_energia
+	MOV  R0, ENERGIA_MÁXIMA		; valor máximo de energia	
+	CMP  R11, R0				;
+	JGE  sai_incremento			; energia máxima atingida?
+    ADD  R11, 1					; incrementa o valor da energia
+    CALL mostra_energia			; mostra energia do rover nos displays
 sai_incremento:
-	POP  R0
-    JMP  ha_tecla
-decremento:
-	CMP  R11, 0
-	JLE  ha_tecla
-    SUB  R11, 1
-    CALL mostra_energia
-    JMP  ha_tecla
+	POP  R0		
+    JMP  ha_tecla		
+decrementa_energia:
+	CMP  R11, ENERGIA_MÍNIMA    ; valor mínimo de energia
+	JLE  ha_tecla 				; energia mínima atingida?
+    SUB  R11, 1					; decrementa o valor da energia
+    CALL mostra_energia 		; mostra energia do rover nos displays
+    JMP  ha_tecla 				;
+
 
 testa_linha_3:
-	CMP	R0, 1 			; tecla 8
+	CMP	R0, COLUNA_1_TECLADO 	; tecla 8
 	JNZ	espera_tecla
-ve_limites_vertical:
-	PUSH R9 
-	PUSH R10 				; 
-	PUSH R11 				;
-
-	MOV R9, MAX_LINHA
-	MOV R11, R8
-	ADD R10, 2
-	MOV R10, [R10]
-	ADD R11, R10
-	CMP R11, R9
-	JGT sai_ve_limites_vertical		; vê se chegou ao limite inferior do ecrã
-
-	POP R11
-	POP R10
-	POP R9
-	ADD R8, 1 				; proxima linha
-	JMP move_meteoro
-sai_ve_limites_vertical:
-	POP R11
-	POP R10
-	POP R9
-	JMP espera_tecla
+	CALL testa_limites_vertical
+	CMP R3, 0
+	JZ  espera_tecla
+	ADD R8, R3
+	CALL move_meteoro
+	JMP ha_tecla
 
 
 ; ******************************************************************************
@@ -292,50 +238,6 @@ sai_desenha_boneco:
 
 
 ; ******************************************************************************
-; APAGA_BONECO - Apaga um boneco na linha e coluna indicadas
-;			  com a forma definida na tabela indicada.
-; Argumentos:   R1 - linha
-;               R2 - coluna
-;               R4 - tabela que define o boneco
-;
-; ******************************************************************************
-apaga_boneco:
-	PUSH	R1
-	PUSH	R2
-	PUSH	R3
-	PUSH	R4
-	PUSH	R5
-	PUSH	R6
-	PUSH 	R7
-	MOV	R5, [R4]			; obtém a largura do boneco
-	MOV R7, R5				; cópia da largura do boneco
-	ADD	R4, 2				; endereço da da altura (2 porque a largura é uma word)
-	MOV R6, [R4]			; obtém a altura do boneco
-apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
-	MOV	R3, 0				; cor para apagar o próximo pixel do boneco
-	CALL	escreve_pixel	; escreve cada pixel do boneco
-    ADD  R2, 1              ; próxima coluna
-    SUB  R5, 1				; menos uma coluna para tratar
-    JNZ  apaga_pixels      	; continua até percorrer toda a largura do objeto
-ab_proxima_linha:
-	MOV  R5, R7  ; obtém a largura do boneco
-	SUB  R2, R5		 	   ; proxima coluna
-	SUB  R6, 1 		   	   ; ultima linha?
-	JZ   sai_apaga_boneco  ;
-	ADD  R1, 1 		   	   ; proxima linha
-	JMP  apaga_pixels
-sai_apaga_boneco:
-	POP R7
-	POP R6
-	POP	R5
-	POP	R4
-	POP	R3
-	POP	R2
-	POP R1
-	RET
-
-
-; ******************************************************************************
 ; ESCREVE_PIXEL - Escreve um pixel na linha e coluna indicadas.
 ; Argumentos:   R1 - linha
 ;               R2 - coluna
@@ -365,7 +267,7 @@ ciclo_atraso:
 
 
 ; ******************************************************************************
-; TESTA_LIMITES - Testa se o boneco chegou aos limites do ecrã e nesse caso
+; TESTA_LIMITES_HORIZONTAL - Testa se o boneco chegou aos limites do ecrã e nesse caso
 ;			   inverte o sentido de movimento
 ; Argumentos:	R2 - coluna em que o objeto está
 ;			R6 - largura do boneco
@@ -375,7 +277,7 @@ ciclo_atraso:
 ; Retorna: 	R7 - novo sentido de movimento (pode ser o mesmo)
 ;
 ; ******************************************************************************
-testa_limites:
+testa_limites_horizontal:
 	PUSH	R5
 	PUSH	R6
 	MOV	R6, [R4]			; obtém a largura do boneco
@@ -399,6 +301,35 @@ impede_movimento:
 sai_testa_limites:	
 	POP	R6
 	POP	R5
+	RET
+
+
+; ******************************************************************************
+; MUDAR - Desenha um boneco na linha e coluna indicadas
+;			    com a forma e cor definidas na tabela indicada.
+; Argumentos:   R1 - linha
+;               R2 - coluna
+;               R4 - tabela que define o boneco
+;
+; ******************************************************************************
+testa_limites_vertical:
+	PUSH R9 
+	PUSH R10 				; 
+	PUSH R11 				;
+
+	MOV R3, 0
+	MOV R9, MAX_LINHA
+	MOV R11, R8
+	ADD R10, 2
+	MOV R10, [R10]
+	ADD R11, R10
+	CMP R11, R9
+	JGT sai_testa_limites_vertical		; vê se chegou ao limite inferior do ecrã
+	MOV R3, 1 				; proxima linha
+sai_testa_limites_vertical:
+	POP R11
+	POP R10
+	POP R9
 	RET
 
 
@@ -428,7 +359,7 @@ teclado:
 
 
 ; ******************************************************************************
-; MOSTRA_ENERGIA - Mostra a energia do rover em percentagem do valor inicial
+; MOSTRA_ENERGIA - Mostra a energia do rover em percentagem do valor inicial nos displays
 ; Argumentos:	R11 - percentagem do valor inicial da energia (em decimal)
 ;
 ; ******************************************************************************
@@ -458,4 +389,56 @@ display:
 	POP R2
 	POP R1
 	POP R11
+	RET
+
+
+; ******************************************************************************
+; MUDAR - Mostra a energia do rover em percentagem do valor inicial nos displays
+; Argumentos:	R11 - percentagem do valor inicial da energia (em decimal)
+;
+; ******************************************************************************
+move_rover:
+	PUSH R10
+	CALL	atraso 			; ciclo para implementar um atraso
+	MOV  R10, 0
+	MOV  [APAGA_ECRÃ], R10  ; apaga o rover
+	ADD	R2, R7				; para desenhar objeto na coluna seguinte (direita ou esquerda)
+	CALL	desenha_boneco	 ; desenha o rover a partir da tabela
+	POP  R10
+	RET
+
+
+; ******************************************************************************
+; MUDAR - Desenha um boneco na linha e coluna indicadas
+;			    com a forma e cor definidas na tabela indicada.
+; Argumentos:   R1 - linha
+;               R2 - coluna
+;               R4 - tabela que define o boneco
+;
+; ******************************************************************************
+move_meteoro:
+	PUSH  R1
+	PUSH  R2
+	PUSH  R3
+	PUSH  R4
+	PUSH R10
+
+	CALL atraso 			 ; ciclo para implementar um atraso
+	MOV  R10, 1
+	MOV  [APAGA_ECRÃ], R10 		; apaga ecrã 1
+	MOV  [SELECIONA_ECRÃ], R10  ; seleciona ecrã 1
+	POP  R10
+
+	MOV   R3, 0
+	MOV   R1, R8					; cópia linha
+	MOV   R2, R9					; cópia coluna
+	MOV   R4, R10					; cópia DEF_METEORO
+	CALL  desenha_boneco			; desenha o meteoro a partir da tabela
+	MOV   [TOCA_SOM], R3			; comando para tocar o som
+	MOV   [SELECIONA_ECRÃ], R3   	; seleciona ecrã 0
+
+	POP R4
+	POP R3
+	POP R2
+	POP R1
 	RET
